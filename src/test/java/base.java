7d4d46d668b5f16b304ff8e6bb0055c66ea20dc3
bednarowicz <bednarowiczr@gmail.com>
2023-11-10 18:24:12 +0100

    dynamicControls task (working but prob should be refactored)

--- src/test/java/base.java
+++ src/test/java/base.java
@@ -3,14 +3,18 @@ import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class base {
     protected WebDriver driver;
+
     @Before
-    public void setUp(){
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -18,15 +22,30 @@ public class base {
         driver.get("http://the-internet.herokuapp.com/");
 
     }
-    public void goToPage(String text){
+
+    public void goToPage(String text) {
         driver.findElement(By.xpath("//ul//a[text()='" + text + "']")).click();
     }
-    public boolean isAlertPresent(){
-        try{
+
+    public boolean isAlertPresent() {
+        try {
             driver.switchTo().alert();
             return true;
-        }catch(NoAlertPresentException ex){
+        } catch (NoAlertPresentException ex) {
             return false;
         }
+    }
+    public void waitForElementToExist(WebDriver driver, WebElement element, int waitTime) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitTime));
+        wait.until(ExpectedConditions.visibilityOf(element));
+
+    }
+    public void waitForElementToExist(WebDriver driver, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+    public void waitForElemntToBeClickable(WebDriver driver, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 }
--- src/test/java/quests.java
+++ src/test/java/quests.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.checkerframework.common.initializedfields.qual.EnsuresInitializedFields;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,9 +9,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.util.LinkedList;
 import java.util.List;
 
-public class quests extends base{
+public class quests extends base {
     @Test
-    public void ABTask(){
+    public void ABTask() {
         Actions actions = new Actions(driver);
         goToPage("A/B Testing");
         String stringAdresOtwartejStrony = driver.getCurrentUrl();
@@ -19,64 +20,69 @@ public class quests extends base{
         Assert.assertTrue("zły tekst wewnątrz strony", tekstNaStronie.contains("Also known as split testing. This is a way in which businesses are able to simultaneously test and learn different versions of a page to see which text and/or functionality works best towards a desired outcome (e.g. a user action such as a click-through)."));
         driver.navigate().back();
     }
+
     @Test
-    public void AddingRemovingElements(){
+    public void AddingRemovingElements() {
         goToPage("Add/Remove Elements");
         int liczbaDodanychElementow = 5;
-        for (int i = 0; i < liczbaDodanychElementow; i++){
+        for (int i = 0; i < liczbaDodanychElementow; i++) {
             driver.findElement(By.xpath("//button[@onclick='addElement()']")).click();
         }
         Assert.assertTrue(driver.findElements(By.xpath("//button[@class='added-manually']")).size() == liczbaDodanychElementow);
-        for (int i = 0; i < liczbaDodanychElementow; i++){
+        for (int i = 0; i < liczbaDodanychElementow; i++) {
             driver.findElement(By.xpath("//button[@onclick='deleteElement()']")).click();
         }
         Assert.assertTrue(driver.findElements(By.xpath("//button[@class='added-manually']")).size() == 0);
     }
+
     @Test
-    public void basicCredentials(){
+    public void basicCredentials() {
         HasAuthentication authentication = (HasAuthentication) driver;
-        authentication.register(()-> new UsernameAndPassword("admin", "admin"));
+        authentication.register(() -> new UsernameAndPassword("admin", "admin"));
         goToPage("Basic Auth");
         Assert.assertTrue(driver.findElement(By.cssSelector("p")).getText().contains("Congratulations! You must have the proper credentials."));
         driver.navigate().back();
     }
+
     @Test
-    public void brokenImage(){
+    public void brokenImage() {
         goToPage("Broken Images");
         List<WebElement> images = driver.findElements(By.tagName("img"));
         List<WebElement> corruptImages = new LinkedList<>();
         List<WebElement> correctImages = new LinkedList<>();
-        for(WebElement image:images){
-            if (image.getAttribute("naturalHeight").equals("0")){
+        for (WebElement image : images) {
+            if (image.getAttribute("naturalHeight").equals("0")) {
                 corruptImages.add(image);
             } else {
                 correctImages.add(image);
             }
         }
-        if (corruptImages.size() != 0){
+        if (corruptImages.size() != 0) {
             System.out.println("Below images are corrupted");
-            for (WebElement image:corruptImages){
+            for (WebElement image : corruptImages) {
                 System.out.println(image.getAttribute("src"));
             }
         }
-        if (corruptImages.size() != 0){
+        if (corruptImages.size() != 0) {
             System.out.println("Below images are fine");
-            for (WebElement image:correctImages){
+            for (WebElement image : correctImages) {
                 System.out.println(image.getAttribute("src"));
             }
         }
 
     }
+
     @Test
-    public void challengingDOM(){
+    public void challengingDOM() {
         //mock up, will be completed later on
     }
+
     @Test
-    public void Checkboxes(){
+    public void Checkboxes() {
         goToPage("Checkboxes");
         List<WebElement> checkBoxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
-        for (WebElement checkBox:checkBoxes){
-            if (checkBox.isSelected()){
+        for (WebElement checkBox : checkBoxes) {
+            if (checkBox.isSelected()) {
                 checkBox.click();
             }
         }
@@ -84,8 +90,9 @@ public class quests extends base{
         Assert.assertTrue(checkBoxes.get(0).isSelected());
         Assert.assertFalse(checkBoxes.get(1).isSelected());
     }
+
     @Test
-    public void contextMenu(){
+    public void contextMenu() {
         goToPage("Context Menu");
         Actions actions = new Actions(driver);
         actions.contextClick(driver.findElement(By.id("hot-spot"))).perform();
@@ -95,60 +102,134 @@ public class quests extends base{
         Assert.assertFalse(isAlertPresent());
 
     }
+
     @Test
-    public void digestAuthentication(){
+    public void digestAuthentication() {
         HasAuthentication authentication = (HasAuthentication) driver;
-        authentication.register(()-> new UsernameAndPassword("admin", "admin"));
+        authentication.register(() -> new UsernameAndPassword("admin", "admin"));
         goToPage("Digest Authentication");
         Assert.assertTrue(driver.findElement(By.xpath("//p")).getText().contains("Congratulations! You must have the proper credentials."));
     }
+
     @Test
-    public void disappearingElements(){
+    public void disappearingElements() {
         goToPage("Disappearing Elements");
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             try {
                 driver.findElement(By.xpath("//a[text()='Gallery']")).click();
                 break;
-            } catch ( Exception e) {
+            } catch (Exception e) {
                 System.out.println("Gallery was not visible. Let's refresh site.");
                 driver.navigate().refresh();
             }
         }
         Assert.assertTrue(driver.getCurrentUrl().equals("http://the-internet.herokuapp.com/gallery/"));
     }
+
     @Test
-    public void dragAndDrop(){
+    public void dragAndDrop() {
         goToPage("Drag and Drop");
         Actions actions = new Actions(driver);
         actions.dragAndDrop(driver.findElement(By.id("column-a")), driver.findElement(By.id("column-b"))).perform();
         Assert.assertTrue(driver.findElement(By.id("column-a")).getText().equals("B"));
         Assert.assertTrue(driver.findElement(By.id("column-b")).getText().equals("A"));
     }
+
     @Test
-    public void dropDown(){
+    public void dropDown() {
         goToPage("Dropdown");
         String chosenOption = "Option 2";
         driver.findElement(By.id("dropdown")).sendKeys(chosenOption);
         Assert.assertTrue(!driver.findElement(By.xpath("//select/option[@value='2']")).getAttribute("selected").isEmpty());
     }
+
     @Test
-    public void dynamicContent(){
+    public void dynamicContent() {
         goToPage("Dynamic Content");
         int tryAmount = 5;
         List<WebElement> images = driver.findElements(By.tagName("img"));
-        for (int i = 0; i < tryAmount; i++){
+        for (int i = 0; i < tryAmount; i++) {
             System.out.println(images.get(3).getAttribute("src"));
-            if (images.get(3).getAttribute("src").toString().endsWith("/img/avatars/Original-Facebook-Geek-Profile-Avatar-7.jpg")){
+            if (images.get(3).getAttribute("src").toString().endsWith("/img/avatars/Original-Facebook-Geek-Profile-Avatar-7.jpg")) {
                 System.out.println("Female joker appeared in 3rd picture.");
                 break;
             } else {
-                System.out.println("Looking for joker picture, driver will click button");
-                driver.findElement(By.xpath("//p//a[text()='click here']")).click();
-                images = driver.findElements(By.tagName("img"));
-            }
-            if (i == (tryAmount-1)){
-                System.out.println("Joker escaped.");
+                if (i != (tryAmount - 1)) {
+                    System.out.println("Looking for joker picture, driver will click button");
+                    driver.findElement(By.xpath("//p//a[text()='click here']")).click();
+                    images = driver.findElements(By.tagName("img"));
+                }
+                if (i == (tryAmount - 1)) {
+                    System.out.println("Joker escaped.");
+                }
             }
         }
+    }
+    @Test
+    public void dynamicControls(){
+        goToPage("Dynamic Controls");
+
+        String xpathCheckbox = "//input[@type='checkbox']" ; //checking checkbox state
+        WebElement checkbox = driver.findElement(By.xpath(xpathCheckbox));
+        Assert.assertTrue(checkbox.isEnabled());
+
+        String xpathRemoveButton = "//button[text()='Remove']"; //clicking remove button
+        WebElement removeButton = driver.findElement(By.xpath(xpathRemoveButton));
+        removeButton.click();
+
+        String xpathLoading = "//div[@id='loading']"; // checking loading sign
+        Assert.assertTrue(driver.findElement(By.xpath(xpathLoading)).isDisplayed());
+
+        String xpathAddButton = "//button[text()='Add']"; // using add button
+        WebElement addButton = driver.findElement(By.xpath(xpathAddButton));
+        waitForElementToExist(driver, addButton);
+        Assert.assertTrue(addButton.isEnabled());
+        addButton.click();
+
+
+        removeButton = driver.findElement(By.xpath(xpathRemoveButton)); // using remove button
+        waitForElementToExist(driver,removeButton);
+        removeButton.click();
+
+        addButton = driver.findElement(By.xpath(xpathAddButton)); // ussing add button
+        waitForElementToExist(driver, addButton);
+        Assert.assertTrue(addButton.isEnabled());
+        addButton.click();
+
+        checkbox = driver.findElement(By.xpath(xpathCheckbox)); //using checkbox
+        waitForElemntToBeClickable(driver, checkbox);
+        checkbox.click();
+        Assert.assertTrue(checkbox.isSelected());
+
+        //let's take care of second part
+
+        String xpathFillableField =  "//input[@type='text']";
+        WebElement fillableField = driver.findElement(By.xpath(xpathFillableField));
+        Assert.assertFalse(fillableField.isEnabled());
+
+        String xpathButton = "//form[@id='input-example']/button";
+        WebElement enableButton = driver.findElement(By.xpath(xpathButton));
+        enableButton.click();
+
+        String xpathEnableDisable = "//p[@id='message']";
+        String xpathEnable = "//p[@id='message' and text()='Enable']";
+        String xpathDisable = "//p[@id='message' and text()='Disable']";
+        WebElement isEnabledText = driver.findElement(By.xpath(xpathEnableDisable));
+        waitForElementToExist(driver, isEnabledText);
+        Assert.assertTrue(isEnabledText.getText().contains("It's enabled!"));
+
+        fillableField = driver.findElement(By.xpath(xpathFillableField));
+        fillableField.sendKeys("Test");
+        Assert.assertTrue(fillableField.getAttribute("value").equals("Test"));
+
+        //String xpathDisableButton = "//button[text()='Disable']";
+
+        WebElement disableButton = driver.findElement(By.xpath(xpathButton));
+        Assert.assertTrue(disableButton.getText().equals("Disable"));
+        disableButton.click();
+
+        enableButton = driver.findElement(By.xpath(xpathButton));
+        waitForElemntToBeClickable(driver, enableButton);
+        Assert.assertFalse(fillableField.isEnabled());
     }
 }
